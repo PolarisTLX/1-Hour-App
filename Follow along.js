06ae622cd293dd445359fec65e66a80d9caeb43d
Paul Rail <paul.a.rail@gmail.com>
2017-11-17 01:03:47 -0500

    Code from 1-hour-app challenge webinar

---
+++ Follow along.js
@@ -0,0 +1,14 @@
+/*using OMDB API for movies
+and PhoneGap from PhoneGap.com to make this work on mobile devices Android and iOS
+
+when you create a new project it will create the skeleton of the project on your computer
+
+
+the www folder contains our starting point
+ App will be writting right in the .html file
+
+ using jQuery mobile version to make it look more and behave more like a mobile app
+
+*/
+
+var key = ;  this is the personal key from the free OMDB API key that you must register for
